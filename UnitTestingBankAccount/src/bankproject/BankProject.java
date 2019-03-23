34064e1955147a625d6b25fd5de439f9804cf37e
sspangsberg <smsj@easv.dk>
2019-03-23 11:45:02 +0100

    Added Unit testing example - BankAccount

---
+++ UnitTestingBankAccount/src/bankproject/BankProject.java
@@ -0,0 +1,15 @@
+package bankproject;
+
+/**
+ *
+ * @author spangsberg
+ */
+public class BankProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }    
+}
