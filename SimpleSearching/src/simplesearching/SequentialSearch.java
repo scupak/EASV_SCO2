b2148f6b59921ebd685a162984f92a9b8262b6c0
sspangsberg <smsj@easv.dk>
2019-04-07 22:47:07 +0200

    Generic Types Fun

--- SimpleSearching/src/simplesearching/SequentialSearch.java
+++ SimpleSearching/src/simplesearching/SequentialSearch.java
@@ -8,6 +8,11 @@ import java.util.List;
  */
 public class SequentialSearch implements SearchStrategy {
 
+    /*
+    Using "bounded type parameters" with <T extends Comparable<T>>
+    doSearch() only accept types that either implements the Comparable interface 
+    or extends a class that implements the Comparable interface
+    */
     @Override
     public <T extends Comparable<T>> int doSearch(List<T> objects, T wanted) {        
         for (int i = 0; i < objects.size(); i++) {
@@ -15,5 +20,15 @@ public class SequentialSearch implements SearchStrategy {
                 return i;
         }        
         return -1;
-    }    
+    }   
+    
+    //Non-generic sequential search (tied to specific type ComputerGame)
+//    @Override
+//    public int doSearch(List<ComputerGame> objects, ComputerGame wanted) {        
+//        for (int i = 0; i < objects.size(); i++) {
+//            if (objects.get(i).equals(wanted))
+//                return i;
+//        }        
+//        return -1;
+//    }    
 }
